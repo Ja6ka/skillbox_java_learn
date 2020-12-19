18bec98b5a42a081b8863cb31939b3f2ff5ee865
Daniil Zhvakin <da.zhvakin@gmail.com>
2020-12-19 20:51:09 +0500

    modified Loader, created getKitten and modified Cat constructor

--- 03_ObjectsAndClasses/Cat/src/Cat.java
+++ 03_ObjectsAndClasses/Cat/src/Cat.java
@@ -5,105 +5,105 @@ public class Cat
     public static final int MIN_WEIGHT = 1000;
     public static final int MAX_WEIGHT = 9000;
 
-    public double originWeight;
-    public double weight;
-    public double minWeight;
-    public double maxWeight;
-    public double foodAmount;
-    public double drinkAmount;
-    public int timesMeow;
+    public static double originWeight;
+    public static double weight;
+    public static double minWeight;
+    public static double maxWeight;
+    public static double foodAmount;
+    public static double drinkAmount;
+    public static int timesMeow;
     public static int count;
-    public boolean isDead = false;
+    public static boolean isDead = false;
     public String colour;
+    public static String name;
 
     //==============================================================================
-    public Cat()
-    {
+
+    public Cat() {
         weight = 1500.0 + 3000.0 * Math.random();
         originWeight = weight;
         minWeight = 1000.0;
         maxWeight = 9000.0;
         count++;
-        ;
-
     }
 
-    public void meow()
-    {
-        if(weight > minWeight && weight < maxWeight && !isDead)
-        {
+    public Cat(double weight) {
+        Cat.weight = weight;
+    }
+
+    public static String setName(String name) {
+        Cat.name = name;
+        return name;
+    }
+
+    public static String getName() {
+        Cat.name = name;
+        return name;
+    }
+
+
+    public static void meow() {
+        if(weight > minWeight && weight < maxWeight) {
             weight = weight - 50;
             timesMeow = ++timesMeow;
             System.out.println("Meow");
-        }
-        else {
+        } else {
             System.out.println("Dead cat cannot meow");
             isDead = true;
             count--;
         }
     }
 
-    public void feed(Double amount)
-    {
-        if(weight > minWeight && weight < maxWeight && !isDead)
-        {
+    public static void feed(Double amount) {
+        if(weight > minWeight && weight < maxWeight) {
             weight = weight + amount;
             foodAmount = foodAmount + amount;
             System.out.println("Yum");
-
-        }
-        else {
+        } else {
             System.out.println("Dead cat cannot eat");
             isDead = true;
             count--;
         }
     }
 
-    public void drink(Double amount)
-    {
-        if(weight > minWeight && weight < maxWeight && !isDead)
-        {
+    public static void drink(Double amount) {
+        if(weight > minWeight && weight < maxWeight) {
             weight = weight + amount;
             drinkAmount = drinkAmount + amount;
             System.out.println("Surp");
-        }
-        else {
+        } else {
             System.out.println("Dead cat cannot drink");
             isDead = true;
             count--;
         }
     }
 
-    public void pee ()
-        {
-            if(weight > minWeight && weight < maxWeight && !isDead)
-            {
+    public static void pee () {
+            if(weight > minWeight && weight < maxWeight) {
                 weight = weight - 100;
                 System.out.println("Peeee");
-            }
-            else {
+            } else {
                 System.out.println("Dead cat cannot pee");
                 isDead = true;
                 count--;
             }
         }
 
-    public int getTimesMeow()
-    {
+    public int getTimesMeow() {
         return timesMeow;
     }
 
-    public Double getFoodAmount()
+    public static Double getFoodAmount()
     {
         return foodAmount;
     }
 
-    public Double getDrinkAmount()
+    public static Double getDrinkAmount()
     {
         return drinkAmount;
     }
 
-    public Double getWeight()
+    public static Double getWeight()
 
     {
         return weight;
@@ -114,7 +114,7 @@ public class Cat
         return count;
     }
 
-    public String getStatus()
+    public static String getStatus()
     {
         if(weight < minWeight)
         {
--- 03_ObjectsAndClasses/Cat/src/Loader.java
+++ 03_ObjectsAndClasses/Cat/src/Loader.java
@@ -8,16 +8,35 @@ public class Loader
 
         System.out.println("Сейчас живо кошек: " + Cat.getDeadCatCount());
 
-        while (!rex.isDead)
+        while (!Cat.isDead)
         {
-            rex.meow();
+            Cat.meow();
         }
 
-        System.out.println(rex.getStatus());
-        System.out.println(rex.getWeight());
+        System.out.println(Cat.getStatus());
+        System.out.println(Cat.getWeight());
         System.out.println("Кошка мяукнула " + rex.getTimesMeow() + " раз перед смертью.");
         System.out.println("Сейчас живо кошек: " + Cat.getDeadCatCount());
+
+        getKitten();
+        Cat.setName("Barsik");
+        System.out.println(Cat.getName() + " is now " + Cat.getStatus() + " and weights " + Cat.getWeight());
+
+        getKitten();
+        Cat.setName("Karasik");
+        System.out.println(Cat.getName() + " is now " + Cat.getStatus() + " and weights " + Cat.getWeight());
+
+        getKitten();
+        Cat.setName("Pupsik");
+        System.out.println(Cat.getName() + " is now " + Cat.getStatus() + " and weights " + Cat.getWeight());
+        Cat.meow();
+        System.out.println(Cat.getWeight());
+
+
     }
 
+    private static Cat getKitten() {
+      return new Cat (1100.00);
+    }
 
 }
