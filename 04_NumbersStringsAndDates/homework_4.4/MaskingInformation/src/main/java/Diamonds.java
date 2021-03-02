22b17ce48654926892952b06f864010b5ae7df1a
Daniil Zhvakin <da.zhvakin@gmail.com>
2021-03-02 21:19:30 +0500

    added Diamonds class

--- 04_NumbersStringsAndDates/homework_4.4/MaskingInformation/src/main/java/Diamonds.java
+++ 04_NumbersStringsAndDates/homework_4.4/MaskingInformation/src/main/java/Diamonds.java
@@ -1,12 +1,19 @@
+import net.sf.saxon.style.XSLOutput;
+
 public class Diamonds {
 
     public static void main(String[] args) {
-
+        String safe = searchAndReplaceDiamonds("Номер кредитной карты <1234 6748 4638> 4654", "***");
+        System.out.println(safe);
     }
-
     public static String searchAndReplaceDiamonds(String text, String placeholder) {
-        // TODO: реализовать метод, если в строке нет <> - вернуть строку без изменений
+        if (!text.contains("<") || !text.contains(">") || text.contains("<<") || text.contains(">>")) {
+            System.out.println(text);
+            return text;
+        }
+        int firstDiamond = text.indexOf("<");
+        int secondDiamond = text.indexOf(">");
+        text = text.substring(0, firstDiamond) + placeholder + text.substring(secondDiamond + 1);
         return text;
     }
-
 }
