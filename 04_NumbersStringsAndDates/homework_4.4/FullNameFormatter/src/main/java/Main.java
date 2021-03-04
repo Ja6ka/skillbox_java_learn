2375b3ce4ff0f28dc4add81f52cafff770eab0c8
Daniil Zhvakin <da.zhvakin@gmail.com>
2021-03-04 19:49:56 +0500

    modified FullNameFormatter

--- 04_NumbersStringsAndDates/homework_4.4/FullNameFormatter/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.4/FullNameFormatter/src/main/java/Main.java
@@ -11,21 +11,38 @@ public class Main {
                 System.out.println("Введенная строка не является ФИО");
                 break;
             }
+
             String fio = input.trim();
 
-            int indexOfSpace1 = fio.indexOf(' ');
-            int indexOfSpace2 = fio.lastIndexOf(' ');
+            int digit = 0;
 
-            String name = fio.substring(indexOfSpace1, indexOfSpace2);
-            String surname = fio.substring(0, indexOfSpace1);
-            String patronymic = fio.substring(indexOfSpace2);
+                for (int i = 0; i < +fio.length(); i++) {
+                    char c = fio.charAt(i);
+                    if (c >= '0' && c <= '9') {
+                        System.out.println("Введенная строка не является ФИО");
+                         digit = 1;
+                         break;
+                    }
+                }
 
-            if (name.trim().contains(" ")) {
-                System.out.println("Введенная строка не является ФИО");
-                break;
-            }
-
-            System.out.println("Фамилия: " + surname.trim() + "\nИмя: " + name.trim() + "\nОтчество: " + patronymic.trim());
+                if ( digit == 0) {
+                    int indexOfSpace1 = fio.indexOf(' ');
+                    int indexOfSpace2 = fio.lastIndexOf(' ');
+                    String name;
+                    if (fio.contains(" ")) {
+                         name = fio.substring(indexOfSpace1, indexOfSpace2);
+                    } else {
+                        System.out.println("Введенная строка не является ФИО");
+                        break;
+                    }
+                    String surname = fio.substring(0, indexOfSpace1);
+                    String patronymic = fio.substring(indexOfSpace2);
+                    if (name.trim().contains(" ")  || name.equals("")) {
+                        System.out.println("Введенная строка не является ФИО");
+                        break;
+                    }
+                    System.out.println("Фамилия: " + surname.trim() + "\nИмя: " + name.trim() + "\nОтчество: " + patronymic.trim());
+                }
         }
     }
 }
