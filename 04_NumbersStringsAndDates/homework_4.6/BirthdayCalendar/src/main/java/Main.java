c7f395bb60787d7ec798e712b7bdb25f105d79c4
Daniil Zhvakin <da.zhvakin@gmail.com>
2021-03-09 22:01:59 +0500

    added BirthdayCalendar class

--- 04_NumbersStringsAndDates/homework_4.6/BirthdayCalendar/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.6/BirthdayCalendar/src/main/java/Main.java
@@ -1,22 +1,36 @@
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        int day = 31;
+        int day = 20;
         int month = 12;
-        int year = 1990;
+        int year = 1996;
 
         System.out.println(collectBirthdays(year, month, day));
 
     }
 
     public static String collectBirthdays(int year, int month, int day) {
-
-        //TODO реализуйте метод для построения строки в следующем виде
-        //0 - 31.12.1990 - Mon
-        //1 - 31.12.1991 - Tue
-        
-        return "";
+        int count = 0;
+        SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy - E", Locale.ENGLISH);
+        Calendar currentDate = Calendar.getInstance();
+        Calendar birthday = Calendar.getInstance();
+        birthday.set(Calendar.DATE, day);
+        birthday.set(Calendar.MONTH, month - 1);
+        birthday.set(Calendar.YEAR, year);
+        StringBuilder text = new StringBuilder();
+        if (birthday.getTimeInMillis() > currentDate.getTimeInMillis()) {
+            return "";
+        }
+        for (birthday.getWeekYear(); birthday.getTimeInMillis() <= currentDate.getTimeInMillis(); ) {
+            text.append(count).append(" - ").append(format.format(birthday.getTime())).append(System.lineSeparator());
+            count++;
+            birthday.add(Calendar.YEAR, 1);
+        }
+        return text.toString();
     }
 }
