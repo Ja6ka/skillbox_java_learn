7cb7278263992a1b8f489ccecff4d421c1829f5d
Daniil Zhvakin <da.zhvakin@gmail.com>
2021-03-12 14:37:51 +0500

    modified phoneCleaner

--- 04_NumbersStringsAndDates/homework_4.5/PhoneCleaner/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/PhoneCleaner/src/main/java/Main.java
@@ -11,23 +11,23 @@ public class Main {
             if (input.equals("0")) {
                 break;
             }
-            Matcher letter = Pattern.compile("[a-zA-z]").matcher(input);
-            if (letter.find()) {
-                System.out.println("Неверный формат номера");
-                break;
-            }
             StringBuilder number = new StringBuilder();
-            Matcher digits = Pattern.compile("\\d+").matcher(input);
+            Matcher digits = Pattern.compile("^\\+?[7-8][ (-]*[9]([( -]*[\\d][ )-]*){9}$|^[ (-]?[9]([( -]*[\\d][ )-]*){9}$").matcher(input);
             while (digits.find()) {
                 number.append(digits.group());
             }
-            String[] s = number.toString().split("");
-            if (number.length() == 11 && s[0].equals("7")) {
-                System.out.println(number);
-            } else if (number.length() == 11 && s[0].equals("8")) {
-                System.out.println("7" + number.substring(1));
-            } else if (number.length() == 10) {
-                System.out.println("7" + number);
+            StringBuilder normal = new StringBuilder();
+            Matcher tester = Pattern.compile("\\d+").matcher(number);
+            while (tester.find()) {
+                normal.append(tester.group());
+            }
+            String[] s = normal.toString().split("");
+            if (normal.length() == 11 && s[0].equals("7")) {
+                System.out.println(normal);
+            } else if (normal.length() == 11 && s[0].equals("8")) {
+                System.out.println("7" + normal.substring(1));
+            } else if (normal.length() == 10) {
+                System.out.println("7" + normal);
             } else {
                 System.out.println("Неверный формат номера");
             }
