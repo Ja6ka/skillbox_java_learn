0bb87694920af54f10f4d5be533ac3e8be2720fb
Daniil Zhvakin <da.zhvakin@gmail.com>
2021-03-14 18:03:50 +0500

    PhoneCleaner completed

--- 04_NumbersStringsAndDates/homework_4.5/PhoneCleaner/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/PhoneCleaner/src/main/java/Main.java
@@ -3,7 +3,6 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         while (true) {
