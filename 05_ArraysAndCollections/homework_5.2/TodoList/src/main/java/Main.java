6339bc60190cf2f7570718703772d5d5d18974e4
Daniil Zhvakin <da.zhvakin@gmail.com>
2021-03-26 01:29:48 +0500

    added TodoList project

--- 05_ArraysAndCollections/homework_5.2/TodoList/src/main/java/Main.java
+++ 05_ArraysAndCollections/homework_5.2/TodoList/src/main/java/Main.java
@@ -1,7 +1,37 @@
+import java.util.Scanner;
+
 public class Main {
-    private static TodoList todoList = new TodoList();
+
+    private static final TodoList todoList = new TodoList();
+    private static final String LIST = "\\s*LIST\\s*";
+    private static final String ADD = "\\s*ADD\\s+[\\p{Alpha}\\s]+";
+    private static final String ADD_NUMBER = "\\s*ADD\\s+\\d+.+";
+    private static final String EDIT = "\\s*EDIT\\s+\\d+.+";
+    private static final String DELETE = "\\s*DELETE\\s+\\d+";
+    private static final String HELP = "\\s*HELP\\s*";
 
     public static void main(String[] args) {
-        // TODO: написать консольное приложение для работы со списком дел todoList
+        while (true) {
+            System.out.println("Добро пожаловать в список задач!");
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+            String[] inputByWords = input.split("\\s+");
+
+            if (input.matches(LIST)) {
+                todoList.list();
+            } else if (input.matches(ADD)) {
+                todoList.add(input.substring(input.indexOf(inputByWords[1])).trim());
+            } else if (input.matches(ADD_NUMBER)) {
+                todoList.add(Integer.parseInt(inputByWords[1]), input.substring(input.indexOf(inputByWords[2])).trim());
+            } else if (input.matches(EDIT)) {
+                todoList.edit(input.substring(input.indexOf(inputByWords[2])).trim(), Integer.parseInt(inputByWords[1].trim()));
+            } else if (input.matches(DELETE)) {
+                todoList.delete(Integer.parseInt(inputByWords[1].trim()));
+            } else if (input.matches(HELP)) {
+                todoList.help();
+            } else {
+                todoList.error();
+            }
+        }
     }
 }
--- 05_ArraysAndCollections/homework_5.2/TodoList/src/main/java/TodoList.java
+++ 05_ArraysAndCollections/homework_5.2/TodoList/src/main/java/TodoList.java
@@ -2,28 +2,59 @@ import java.util.ArrayList;
 
 public class TodoList {
 
+    private final ArrayList<String> todoList = new ArrayList<>();
+
     public void add(String todo) {
-        // TODO: добавьте переданное дело в конец списка
+        todoList.add(todoList.size(), todo);
+        System.out.println("Добавлено!");
     }
 
     public void add(int index, String todo) {
-        // TODO: добавьте дело на указаный индекс,
-        //  проверьте возможность добавления
+        if (index < todoList.size() && index >= 0) {
+            todoList.add(index, todo);
+            System.out.println("Добавлено!");
+        } else {
+            System.out.println("Ошибка!");
+        }
     }
 
     public void edit(String todo, int index) {
-        // TODO: заменить дело на index переданным todo индекс,
-        //  проверьте возможность изменения
+        if (index < todoList.size()) {
+            todoList.set(index, todo);
+            System.out.println("Дело изменено!");
+        } else {
+            System.out.println("Ошибка!");
+        }
     }
 
     public void delete(int index) {
-        // TODO: удалить дело находящееся по переданному индексу,
-        //  проверьте возможность удаления дела
+        if (index < todoList.size() && index >= 0) {
+            todoList.remove(index);
+        } else {
+            System.out.println("Ошибка!");
+        }
+    }
+
+    public void list() {
+        for (int i = 0; i < todoList.size(); i++) {
+            System.out.println(i + " - " + todoList.get(i));
+        }
+    }
+
+    public void help() {
+        System.out.println("Доступные команды:" + System.lineSeparator() +
+                "\tLIST - выводит весь список задач в консоль" + System.lineSeparator() +
+                "\tADD *название задачи* - добавляет задачу в конец списка" + System.lineSeparator() +
+                "\tADD *номер задачи и название новой задачи* - добавляет задачу в список на место заданного номера" + System.lineSeparator() +
+                "\tEDIT *номер задачи в списке и название новой задачи* - изменяет задачу под заданным номером" + System.lineSeparator() +
+                "\tDELETE *номер задачи в списке* - удаляет задачу под заданным номером");
+    }
+
+    public void error() {
+        System.out.println("Введите команду из списка. Чтобы отобразить список доступных команд введите HELP");
     }
 
     public ArrayList<String> getTodos() {
-        // TODO: вернуть список дел
-        return new ArrayList<>();
+        return todoList;
     }
-
 }
