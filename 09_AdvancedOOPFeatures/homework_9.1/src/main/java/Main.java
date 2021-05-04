0b1c45ec41b10cacc1e08f61a8fc5ac794bbb09a
Daniil Zhvakin <da.zhvakin@gmail.com>
2021-05-04 19:52:40 +0500

    modified LambdaExpressions project

--- 09_AdvancedOOPFeatures/homework_9.1/src/main/java/Main.java
+++ 09_AdvancedOOPFeatures/homework_9.1/src/main/java/Main.java
@@ -6,10 +6,18 @@ public class Main {
 
     public static void main(String[] args) {
         List<Employee> staff = Employee.loadStaffFromFile(STAFF_TXT);
+        sortBySalaryAndAlphabet(staff);
         System.out.println(staff);
     }
 
     public static void sortBySalaryAndAlphabet(List<Employee> staff) {
-        //TODO Метод должен отсортировать сотрудников по заработной плате и алфавиту.
+        staff.sort(((o1, o2) -> {
+            int i;
+            i = o1.getSalary().compareTo(o2.getSalary());
+            if (o1.getSalary().equals(o2.getSalary())) {
+                i = o1.getName().compareTo(o2.getName());
+            }
+            return i;
+        }));
     }
 }
