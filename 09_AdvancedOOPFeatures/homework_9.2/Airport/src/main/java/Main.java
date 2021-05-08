d3044af3fe49df02ff433ce472ba8f1eff8ecb45
Daniil Zhvakin <da.zhvakin@gmail.com>
2021-05-08 18:59:48 +0500

    modified Airport 9.2 hometask

--- 09_AdvancedOOPFeatures/homework_9.2/Airport/src/main/java/Main.java
+++ 09_AdvancedOOPFeatures/homework_9.2/Airport/src/main/java/Main.java
@@ -1,17 +1,27 @@
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
-import java.util.Collections;
-import java.util.List;
+
+import java.util.*;
 
 public class Main {
+
+
     public static void main(String[] args) {
-
-
+        Airport airport = Airport.getInstance();
+        System.out.println(findPlanesLeavingInTheNextTwoHours(airport));
     }
 
     public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
-        //TODO Метод должден вернуть список рейсов вылетающих в ближайшие два часа.
-        return Collections.emptyList();
-    }
 
+        List<Flight> nextTwoHourDepartures = new ArrayList<>();
+        Date currentDate = new Date();
+        Date plusTwoHoursDate = new Date();
+        plusTwoHoursDate.setHours(currentDate.getHours() + 2);
+
+        airport.getTerminals().forEach(terminal -> terminal.getFlights().stream()
+                .filter(t -> t.getType().equals(Flight.Type.DEPARTURE))
+                .filter(t -> t.getDate().after(currentDate) && t.getDate().before(plusTwoHoursDate))
+                .forEach(nextTwoHourDepartures::add));
+        return nextTwoHourDepartures;
+    }
 }
