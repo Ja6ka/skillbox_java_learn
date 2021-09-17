f5a146e3bc37a21644340afb6a1815ee99d28e67
Daniil Zhvakin <da.zhvakin@gmail.com>
2021-09-17 16:12:32 +0500

    modified FilesAndNetwork homework

--- 13_FilesAndNetwork/homework_13.1/src/main/java/FileUtils.java
+++ 13_FilesAndNetwork/homework_13.1/src/main/java/FileUtils.java
@@ -1,6 +1,54 @@
+import java.io.File;
+
 public class FileUtils {
 
     public static long calculateFolderSize(String path) {
-        return 0;
+        long totalLength = 0;
+        try {
+            File file = new File(path);
+            if (file.isDirectory()) {
+                for (File f : file.listFiles()) {
+                    if (f.isDirectory()) {
+                        totalLength += calculateFolderSize(f.getPath());
+                    } else {
+                        totalLength += f.length();
+                    }
+                }
+            } else {
+                totalLength = file.length();
+            }
+        } catch (NullPointerException ex) {
+            ex.printStackTrace();
+        }
+        return totalLength;
+    }
+
+    public static String isFolder(String path) {
+        String type;
+        File file = new File(path);
+        if (file.isDirectory()) {
+            type = "папки";
+        } else {
+            type = "файла";
+        }
+        return type;
+    }
+
+    public static String convertFormat(long size) {
+        String[] suffix = {" байт", " Кбайт", " Мбайт", " Гбайт", " Тбайт"};
+        int index = 0;
+
+        double temp = (double) size / 1024;
+
+        while (temp >= 1) {
+            temp = temp / 1024;
+            index++;
+        }
+
+        if (index == 0) {
+            return size + suffix[index];
+        } else {
+            return (Math.round(10 * size / (Math.pow(1024, (index)))) / 10.0) + suffix[index];
+        }
     }
 }
--- 13_FilesAndNetwork/homework_13.1/src/main/java/Main.java
+++ 13_FilesAndNetwork/homework_13.1/src/main/java/Main.java
@@ -1,5 +1,19 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        for (; ; ) {
+            long size;
+            Scanner scanner = new Scanner(System.in);
 
+            System.out.println("Введите путь к папке или файлу: ");
+            String input = scanner.nextLine().replace('\\', File.separatorChar);
+
+            File file = new File(input);
+            System.out.println(file.length());
+            size = FileUtils.calculateFolderSize(input);
+            System.out.println("Размер " + FileUtils.isFolder(input) + ": " + FileUtils.convertFormat(size));
+        }
     }
 }
