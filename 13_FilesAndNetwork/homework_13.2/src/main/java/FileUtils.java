9c20a624ee2eeec3e5e0becb44506e7fcf33b10d
Daniil Zhvakin <da.zhvakin@gmail.com>
2021-09-26 20:03:04 +0500

    modified FileUtils and Main

--- 13_FilesAndNetwork/homework_13.2/src/main/java/FileUtils.java
+++ 13_FilesAndNetwork/homework_13.2/src/main/java/FileUtils.java
@@ -1,5 +1,25 @@
+import java.io.File;
+import java.nio.file.*;
+
 public class FileUtils {
     public static void copyFolder(String sourceDirectory, String destinationDirectory) {
-        // TODO: write code copy content of sourceDirectory to destinationDirectory
+        File startFolder = new File(sourceDirectory);
+        File destFolder = new File(destinationDirectory);
+
+        try {
+            if (startFolder.isDirectory()) {
+                if (!destFolder.exists()) {
+                    Files.createDirectory(destFolder.toPath());
+                    for (File f : startFolder.listFiles()) {
+                        Files.copy(f.toPath(), destFolder.toPath().resolve(f.getName()), StandardCopyOption.REPLACE_EXISTING);
+                        copyFolder(f.toString(), destFolder.toString());
+                    }
+                }
+            } else {
+                Files.copy(startFolder.toPath(), destFolder.toPath().resolve(startFolder.getName()), StandardCopyOption.REPLACE_EXISTING);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
--- 13_FilesAndNetwork/homework_13.2/src/main/java/Main.java
+++ 13_FilesAndNetwork/homework_13.2/src/main/java/Main.java
@@ -1,4 +1,18 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        for (; ; ) {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Введите путь к папке или файлу, откуда необходимо провести копирование: ");
+            String startFolder = scanner.nextLine().replace('\\', File.separatorChar);
+
+            System.out.println("Введите путь к точке назначения: ");
+            String destFolder = scanner.nextLine().replace('\\', File.separatorChar);
+
+            FileUtils.copyFolder(startFolder, destFolder);
+        }
     }
 }
