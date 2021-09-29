b36d949a4b26a71257b42a8c4f39fcd203184f23
Daniil Zhvakin <da.zhvakin@gmail.com>
2021-09-29 17:15:50 +0500

    modified FileUtils and Main

--- 13_FilesAndNetwork/homework_13.2/src/main/java/FileUtils.java
+++ 13_FilesAndNetwork/homework_13.2/src/main/java/FileUtils.java
@@ -1,25 +1,19 @@
-import java.io.File;
-import java.nio.file.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class FileUtils {
-    public static void copyFolder(String sourceDirectory, String destinationDirectory) {
-        File startFolder = new File(sourceDirectory);
-        File destFolder = new File(destinationDirectory);
-
-        try {
-            if (startFolder.isDirectory()) {
-                if (!destFolder.exists()) {
-                    Files.createDirectory(destFolder.toPath());
-                    for (File f : startFolder.listFiles()) {
-                        Files.copy(f.toPath(), destFolder.toPath().resolve(f.getName()), StandardCopyOption.REPLACE_EXISTING);
-                        copyFolder(f.toString(), destFolder.toString());
+    public static void copyFolder(String sourceDirectory, String destinationDirectory) throws IOException {
+        Files.walk(Paths.get(sourceDirectory))
+                .forEach(source -> {
+                    Path destination = Paths.get(destinationDirectory, source.toString()
+                            .substring(sourceDirectory.length()));
+                    try {
+                        Files.copy(source, destination);
+                    } catch (IOException e) {
+                        e.printStackTrace();
                     }
-                }
-            } else {
-                Files.copy(startFolder.toPath(), destFolder.toPath().resolve(startFolder.getName()), StandardCopyOption.REPLACE_EXISTING);
-            }
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
+                });
     }
 }
--- 13_FilesAndNetwork/homework_13.2/src/main/java/Main.java
+++ 13_FilesAndNetwork/homework_13.2/src/main/java/Main.java
@@ -1,8 +1,9 @@
 import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         for (; ; ) {
             Scanner scanner = new Scanner(System.in);
 
