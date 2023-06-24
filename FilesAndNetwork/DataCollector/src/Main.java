7b0a75528fa486b62633a643b6eecc5ca71cfeb3
Daniil Zhvakin <da.zhvakin@gmail.com>
2023-06-24 23:12:26 +0500

    modified some classes, tried to reach goals with unified parsing method for JSON and CSV files

--- FilesAndNetwork/DataCollector/src/Main.java
+++ FilesAndNetwork/DataCollector/src/Main.java
@@ -3,20 +3,30 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        String htmlFilePath = "https://skillbox-java.github.io";
-        HTMLParser.parseAndPrintLinesAndStations(htmlFilePath);
+//        String htmlFilePath = "https://skillbox-java.github.io";
+//        HTMLParser.parseAndPrintLinesAndStations(htmlFilePath);
+//
+//        String folderPath = "C:\\java_basics\\FilesAndNetwork\\DataCollector\\lib\\data";
+//        JSONAndCSVFinder.searchForCSVAndJSONFiles(folderPath);
+//
+//        String jsonFilePath = "C:\\java_basics\\FilesAndNetwork\\DataCollector\\lib\\data\\data\\0\\5\\dates-2.json";
+//        List<Station> stationsJSON = Station.readJsonFile(jsonFilePath);
+//        if (stationsJSON != null) {
+//            for (Station station : stationsJSON) {
+//                System.out.println("Название станции: " + station.getName());
+//                System.out.println("Дата открытия: " + station.getDate());
+//                System.out.println();
+//            }
+//        }
 
-        String folderPath = "C:\\java_basics\\FilesAndNetwork\\DataCollector\\lib\\data";
-        JSONAndCSVFinder.searchForCSVAndJSONFiles(folderPath);
+        String csvFilePath = "C:\\java_basics\\FilesAndNetwork\\DataCollector\\lib\\data\\data\\4\\6\\dates-1.csv";
+        List<Station> stationsCsv = CSVParser.readCsvFile(csvFilePath);
 
-        String jsonFilePath = "C:\\java_basics\\FilesAndNetwork\\DataCollector\\lib\\data\\data\\0\\5\\dates-2.json";
-        List<Station> stations = Station.readJsonFile(jsonFilePath);
-        if (stations != null) {
-            for (Station station : stations) {
-                System.out.println("Название станции: " + station.getName());
-                System.out.println("Дата открытия: " + station.getDate());
-                System.out.println();
-            }
+        for (Station station : stationsCsv) {
+            System.out.println("Название станции: " + station.getName());
+            System.out.println("Дата открытия: " + station.getDate());
+            System.out.println("Глубина: " + station.getDepth());
+            System.out.println();
         }
     }
 }
