9e46267ecc4f253dda5bc6199c00c6de15f26a08
Daniil Zhvakin <da.zhvakin@gmail.com>
2023-10-20 22:43:13 +0500

    modified Loader class

--- Performance/CarNumberGenerator/src/Loader.java
+++ Performance/CarNumberGenerator/src/Loader.java
@@ -1,41 +1,76 @@
 import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Loader {
 
     public static void main(String[] args) throws Exception {
+        int threadNumber = Runtime.getRuntime().availableProcessors();
+        int regionCount = 199;
+        int regionPartsCount = regionCount / threadNumber;
+
+        ExecutorService executorService = Executors.newFixedThreadPool(threadNumber);
+        int start;
+        int end = 1;
+        for (int i = 1; i <= threadNumber; i++) {
+            start = end;
+            if (i == threadNumber) {
+                end = regionCount + 1;
+            } else {
+                end = start + regionPartsCount;
+            }
+            int finalStart = start;
+            int finalEnd = end;
+            executorService.execute(() -> {
+                try {
+                    generate(finalStart, finalEnd);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+        executorService.shutdown();
+    }
+
+    private static void generate(int regionFrom, int regionTo) throws Exception {
         long start = System.currentTimeMillis();
 
-        FileOutputStream writer = new FileOutputStream("res/numbers.txt");
+        PrintWriter writer = new PrintWriter("res/numbers " + regionFrom + " - " + regionTo + ".txt");
 
-        char letters[] = {'–£', '–ö', '–ï', '–ù', '–•', '–í', '–ê', '–†', '–û', '–°', '–ú', '–¢'};
-        for (int number = 1; number < 1000; number++) {
-            int regionCode = 199;
-            for (char firstLetter : letters) {
-                for (char secondLetter : letters) {
-                    for (char thirdLetter : letters) {
-                        String carNumber = firstLetter + padNumber(number, 3) +
-                            secondLetter + thirdLetter + padNumber(regionCode, 2);
-                        writer.write(carNumber.getBytes());
-                        writer.write('\n');
+        char[] letters = {'”', ' ', '≈', 'Õ', '’', '¬', '¿', '–', 'Œ', '—', 'Ã', '“'};
+
+
+        for (int regionCode = regionFrom; regionCode < regionTo; regionCode++) {
+
+            StringBuilder regionString = new StringBuilder(regionCode);
+            if (regionCode < 10) {
+                regionString.insert(0, '0');
+            }
+
+            StringBuilder builder = new StringBuilder();
+            for (int number = 1; number < 1000; number++) {
+
+                StringBuilder numberString = new StringBuilder(number);
+                if (number < 10) {
+                    numberString.insert(0, "00");
+                } else if (number < 100) {
+                    numberString.insert(0, '0');
+                }
+
+                for (char firstLetter : letters) {
+                    for (char secondLetter : letters) {
+                        for (char thirdLetter : letters) {
+                            builder.append(firstLetter).append(numberString).append(secondLetter)
+                                    .append(thirdLetter).append(regionString).append('\n');
+                        }
                     }
                 }
             }
+            writer.write(builder.toString());
         }
-
         writer.flush();
         writer.close();
-
         System.out.println((System.currentTimeMillis() - start) + " ms");
-    }
-
-    private static String padNumber(int number, int numberLength) {
-        String numberStr = Integer.toString(number);
-        int padSize = numberLength - numberStr.length();
-
-        for (int i = 0; i < padSize; i++) {
-            numberStr = '0' + numberStr;
-        }
-
-        return numberStr;
     }
 }
