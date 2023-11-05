b024f99ef52776eeab5a8beea1dc1041d7490366
Daniil Zhvakin <da.zhvakin@gmail.com>
2023-11-06 00:25:38 +0500

    modified Main class

--- NoSQL/Queue/java/src/Main.java
+++ NoSQL/Queue/java/src/Main.java
@@ -16,19 +16,26 @@ public class Main {
     public static void main(String[] args) {
 
         while (true) {
-            currentUserID = Integer.parseInt(jedis.rpop(KEY));
-            System.out.println("Показываем пользователя - " + currentUserID);
-            jedis.lpush(KEY, String.valueOf(currentUserID));
+            currentUserID = Integer.parseInt(jedis.lpop(KEY));
+            printAndRemoveUserID(currentUserID, false);
             counter++;
 
             if (counter == 10) {
                 paidUserID = random.nextInt(1, 20);
-                jedis.rpush(KEY, String.valueOf(paidUserID));
+                jedis.lpush(KEY, String.valueOf(paidUserID));
                 System.out.println("Пользователь " + paidUserID + " оплатил");
-                System.out.println("Показываем пользователя - " + paidUserID);
-                jedis.rpop(KEY);
+                printAndRemoveUserID(paidUserID, true);
                 counter = 0;
             }
+        }
+    }
+
+    public static void printAndRemoveUserID (int userID, boolean paid) {
+        System.out.println("Показываем пользователя - " + userID);
+        if (paid) {
+            jedis.lpop(KEY);
+        } else {
+            jedis.rpush(KEY, String.valueOf(userID));
         }
     }
 }
